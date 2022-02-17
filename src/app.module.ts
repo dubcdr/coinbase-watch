5bf8bf874d93ef8aecae68fe54ff840235c4608f
Devin Riley <devinriley@gmail.com>
2022-02-17 13:33:47 -0600

    basic eth listen

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import CoinbasePro from 'coinbase-pro-node';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'COINBASE_CLIENT',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const client = new CoinbasePro({
+          apiKey: configService.get('API_KEY'),
+          apiSecret: configService.get('API_SECRET'),
+          passphrase: configService.get('API_PASSPHRASE'),
+          useSandbox: false,
+        });
+        return client;
+      }
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import CoinbasePro, {
+  Candle,
+  CandleGranularity,
+  ProductEvent,
+} from 'coinbase-pro-node';
+import { addDays, addMinutes } from 'date-fns';
+import { interval, Observable, Subject, switchMap, tap, takeUntil } from 'rxjs';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  constructor(@Inject('COINBASE_CLIENT') private client: CoinbasePro) { }
+
+  async onModuleInit() {
+
+    const date = new Date();
+    const startDate = addDays(date, -180);
+    const endDate = addMinutes(date, -60);
+    this.getHistoricData(startDate, endDate).subscribe(historic => {
+      console.log(`Got data for ${historic[0].openTimeInISO}, closing price ${historic[0].close}`);
+    })
+  }
+
+  async setupListen() {
+    const productId = 'ETH-USD';
+    const granularity = CandleGranularity.ONE_MINUTE;
+
+    this.client.rest.on(ProductEvent.NEW_CANDLE,
+      (productId: string, granularity: CandleGranularity, candle: Candle) => {
+        console.info('Recent candle', productId, granularity, candle.openTimeInISO);
+        console.table(candle);
+      })
+
+    // 3. Get latest candle
+    const candles = await this.client.rest.product.getCandles(productId, {
+      granularity,
+    });
+    const latestCandle = candles[candles.length - 1];
+    const latestOpen = latestCandle.openTimeInISO;
+    console.info('Initial candle', productId, granularity, latestOpen);
+    console.table(latestCandle);
+
+    // 4. Subscribe to upcoming candles
+    this.client.rest.product.watchCandles(productId, granularity, latestOpen);
+
+  }
+
+  getHistoricData(start: Date, end: Date): Observable<Candle[]> {
+    const finished = new Subject<void>();
+    let tempEnd = addMinutes(start, 60);
+    return interval(200)
+      .pipe(
+        takeUntil(finished),
+        switchMap(() => {
+          return this.client.rest.product.getCandles('ETH-USD', {
+            granularity: CandleGranularity.ONE_MINUTE,
+            start: start.toISOString(),
+            end: tempEnd.toISOString(),
+          });
+        }),
+        tap(() => {
+
+          start = tempEnd;
+          tempEnd = addMinutes(start, 10);
+
+          if (start > end) {
+            finished.next();
+            finished.complete();
+          }
+
+        })
+      )
   }
 }
